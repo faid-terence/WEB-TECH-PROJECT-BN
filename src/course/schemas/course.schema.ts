f308ea24228601b0796a6b98e2883bcba1329a39
Terence Faid JABO <faidterence@outlook.com>
2023-09-26 20:52:31 +0200

    feat: Implement course retrieval functionality

---
+++ src/course/schemas/course.schema.ts
@@ -0,0 +1,37 @@
+import {Schema, Prop, SchemaFactory} from '@nestjs/mongoose'
+import * as mongoose from 'mongoose'
+export enum Category {
+    WEB_DEVELOPMENT = "webdev",
+    MOBILE_DEVELOPMENT = "mobdev",
+    DATABASE_DEVELOPMENT = "databasedev",
+    PROGRAMMING_LANGUAGES = "programing"
+}
+
+@Schema({
+    timestamps: true
+})
+
+export class Course {
+    @Prop({required: true, unique: true})
+    title: string;
+
+    @Prop()
+    description: string;
+
+    @Prop()
+    lessons: [];
+    @Prop()
+    duration : number;
+
+    @Prop()
+    category: Category
+    @Prop() 
+    price: string;
+
+    @Prop()
+    avaerageRating: number;
+
+}
+
+
+export const CourseSchema = SchemaFactory.createForClass(Course);
