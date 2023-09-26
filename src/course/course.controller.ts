16b80345236a80f5c85e525fe09477c135226a03
Terence Faid JABO <faidterence@outlook.com>
2023-09-26 22:22:25 +0200

    feat:Get a single course by Id

--- src/course/course.controller.ts
+++ src/course/course.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body } from '@nestjs/common';
+import { Controller, Get, Post, Body,Param } from '@nestjs/common';
 import { CourseService } from './course.service';
 import { Course } from './schemas/course.schema';
 import { CreateCourseDto } from './dto/create-course.dto';
@@ -16,5 +16,12 @@ export class CourseController {
         course: CreateCourseDto
     ) : Promise<Course>{
         return this.courseService.addCourse(course);
+    }
+    @Get(':id')
+    async getOneCourse(
+        @Param('id')
+        id: string
+    ) : Promise<Course>{
+        return this.courseService.findOne(id)
     }
 }
