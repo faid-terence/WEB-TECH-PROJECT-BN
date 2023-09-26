7dad7918e1b8010f3a6bc61f21fe0d72e32c4b95
Terence Faid JABO <faidterence@outlook.com>
2023-09-26 20:52:31 +0200

    feat: Implement course retrieval functionality

--- src/course/course.controller.ts
+++ src/course/course.controller.ts
@@ -1,4 +1,12 @@
-import { Controller } from '@nestjs/common';
-
+import { Controller, Get } from '@nestjs/common';
+import { CourseService } from './course.service';
+import { Course } from './schemas/course.schema';
 @Controller('course')
-export class CourseController {}
+export class CourseController {
+    constructor(private courseService : CourseService) {}
+
+    @Get()
+    async getAllCourses() : Promise<Course[]>{
+        return this.courseService.findAll();
+    }
+}
--- src/course/course.service.ts
+++ src/course/course.service.ts
@@ -1,4 +1,18 @@
 import { Injectable } from '@nestjs/common';
-
+import { Course } from './schemas/course.schema';
+import {InjectModel} from '@nestjs/mongoose'
+import * as mongoose from 'mongoose'
+import { promises } from 'dns';
 @Injectable()
-export class CourseService {}
+export class CourseService {
+    constructor(
+        @InjectModel(Course.name)
+        private courseModel : mongoose.Model<Course>
+    ){}
+
+    async findAll() : Promise<Course[]>{
+        const courses = await this.courseModel.find();
+
+        return courses;
+    }
+}
