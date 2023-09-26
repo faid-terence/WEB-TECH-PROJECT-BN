7d52ae19ab3a72a5264f59510f89b6bd94752414
Terence Faid JABO <faidterence@outlook.com>
2023-09-26 19:09:47 +0200

    feat: Set up database connection and folder structure

---
+++ src/app.module.ts
@@ -0,0 +1,17 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { CourseModule } from './course/course.module';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({envFilePath: ".env", isGlobal: true}),
+    MongooseModule.forRoot(process.env.DB_URL),
+
+    CourseModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
