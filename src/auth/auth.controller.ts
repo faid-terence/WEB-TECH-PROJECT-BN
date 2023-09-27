d02462364f71a14511d14a23db94e0e9980660a6
Terence Faid JABO <faidterence@outlook.com>
2023-09-27 19:46:29 +0200

    feat:Implement user registration functionality

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,8 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import {Get} from '@nestjs/common'
 import { User } from './schema/user.schema';
+import { RegisterUserDto } from './dto/create-user.dto';
 
 @Controller('auth')
 export class AuthController {
@@ -10,6 +11,14 @@ export class AuthController {
     @Get()
     async getAllUsers () : Promise<User[]>{
         return this.authService.findUsers();
+    }
+
+    @Post('register')
+    async registerUser(
+        @Body()
+        user: RegisterUserDto
+    ) : Promise<User> {
+        return this.authService.registerUser(user);
     }
 
 }
